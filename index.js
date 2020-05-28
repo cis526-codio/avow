3d5c673f3056447b13457b60a55fcb5d033ab28c
Nathan H. Bean <nhbean@ksu.edu>
2020-05-28 12:15:16 -0500

    removed references to html-parse-stringify

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 "use strict";
 
 const avowCSS = require('./src/avow-css');
-const avowHTML = require('./src/avow-html');
+//const avowHTML = require('./src/avow-html');
 const formatIssues = require('./src/format-issues');
 
 module.exports = {
     css: avowCSS,
-    html: avowHTML,
+    /* html: avowHTML, dependency issues with phantomjs */
     formatIssues: formatIssues
 }
