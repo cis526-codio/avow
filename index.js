13f47d2e404f8b064dbbdc339d927094d28826d7
Nathan Bean <nhbean@ksu.edu>
2020-06-10 11:51:47 -0500

    Added pass/fail functions to index

--- index.js
+++ index.js
@@ -3,9 +3,13 @@
 const avowCSS = require('./src/avow-css');
 //const avowHTML = require('./src/avow-html');
 const formatIssues = require('./src/format-issues');
+const pass = require('./src/pass');
+const fail = require('./src/fail');
 
 module.exports = {
     css: avowCSS,
     /* html: avowHTML, dependency issues with phantomjs */
+    pass: pass,
+    fail: fail,
     formatIssues: formatIssues
 }
