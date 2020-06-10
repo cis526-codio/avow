f40bad1fd44b8a3fae233063afccbd2ede869a34
Nathan H. Bean <nhbean@ksu.edu>
2020-06-10 10:50:48 -0500

    Added export to format-issues.js

--- src/format-issues.js
+++ src/format-issues.js
@@ -1,4 +1,4 @@
-/** @fucntion formatIssues 
+/** @function formatIssues 
  * Formats an issue string list into an unordered html list
  * @param {string[]} issues - the issues to report 
  * @returns {string} the HTML snippet
@@ -7,3 +7,5 @@ function formatIssues(issues) {
     var issueItems = issues.map(issue => `<li>${issue}</li>`).join('\n');
     return `<ul>\n${issueItems}\n</ul>`;
 }
+
+module.exports = formatIssues;
